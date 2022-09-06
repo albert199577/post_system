a3fc5ec2665cf3bcf70400dcf53355dd61e697d2
albert.kao <albert.kao@udngroup.com>
2022-09-06 17:30:40 +0800

    deleting related model use model events

--- app/app/app/Models/BlogPost.php
+++ app/app/app/Models/BlogPost.php
@@ -15,4 +15,13 @@ class BlogPost extends Model
     {
         return $this->hasMany('App\Models\Comment');
     }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function (BlogPost $blogPost) {
+            $blogPost->comments()->delete();
+        });
+    }
 }
