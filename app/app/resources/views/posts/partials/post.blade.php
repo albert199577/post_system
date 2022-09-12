5e7ff4dd367af9ac28efe194e657041733963738
albert.kao <albert.kao@udngroup.com>
2022-09-12 16:13:58 +0800

    feat: give admin authorize

--- app/app/resources/views/posts/partials/post.blade.php
+++ app/app/resources/views/posts/partials/post.blade.php
@@ -14,10 +14,19 @@
 @endif
 
 <div class="d-flex mb-3">
-    <a class="btn btn-success text-light" href="{{ route('posts.edit', ['post' => $post->id]) }}">Edit</a>
-    <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button class="btn btn-info text-light" type="submit">Delete!</button>
-    </form>
+    @can('update', $post)
+        <a class="btn btn-success text-light" href="{{ route('posts.edit', ['post' => $post->id]) }}">Edit</a>
+    @endcan
+    
+    @can('delete', $post)
+        <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button class="btn btn-info text-light" type="submit">Delete!</button>
+        </form>
+    @endcan
 </div>
+
+@cannot('delete', $post)
+    <p>You can't delete this post</p>
+@endcannot
