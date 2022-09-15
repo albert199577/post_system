d633238849f8910994393727c22113a46cdeac06
albert.kao <albert.kao@udngroup.com>
2022-09-15 09:51:25 +0800

    refactor: component for layout #3

--- app/app/resources/views/posts/index.blade.php
+++ app/app/resources/views/posts/index.blade.php
@@ -26,7 +26,20 @@
     <div class="col-4">
         <div class="container">
             <div class="row">
-                <div class="card" style="width: 100%;">
+                <x-card title="Most Commented"
+                        subtitle="What people are currently talking about"
+                        >
+                    @slot('items')
+                        @foreach ($mostCommented as $post)
+                            <li class="list-group-item">
+                                <a href="{{ route('posts.show', ['post' => $post->id]) }}">
+                                    {{ $post->title }}
+                                </a>
+                            </li>
+                        @endforeach
+                    @endslot
+                </x-card>
+                {{-- <div class="card" style="width: 100%;">
                     <div class="card-body">
                         <h5 class="card-title">Most Commented</h5>
                         <h6 class="card-subtitle mb-2 text-muted">What people are currently talking about</h6>
@@ -40,11 +53,15 @@
                             </li>
                         @endforeach
                     </ul>
-                </div>
+                </div> --}}
             </div>
-
+            
             <div class="row mt-4">
-                <div class="card" style="width: 100%;">
+                <x-card title="Most Active"
+                    subtitle="User with most posts written"
+                    :items="collect($mostActive)->pluck('name')">
+                </x-card>
+                {{-- <div class="card" style="width: 100%;">
                     <div class="card-body">
                         <h5 class="card-title">Most Active</h5>
                         <h6 class="card-subtitle mb-2 text-muted">
@@ -58,11 +75,15 @@
                             </li>
                         @endforeach
                     </ul>
-                </div>
+                </div> --}}
             </div>
-
+            
             <div class="row mt-4">
-                <div class="card" style="width: 100%;">
+                <x-card title="Most Active Last Month"
+                    subtitle="User with most posts written in the last month"
+                    :items="collect($mostActiveUserLastMonth)->pluck('name')">
+                </x-card>
+                {{-- <div class="card" style="width: 100%;">
                     <div class="card-body">
                         <h5 class="card-title">Most Active Last Month</h5>
                         <h6 class="card-subtitle mb-2 text-muted">
@@ -76,7 +97,7 @@
                             </li>
                         @endforeach
                     </ul>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>
