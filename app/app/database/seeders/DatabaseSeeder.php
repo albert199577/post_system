068a498750eada10024489cf66b6dec4ad5babc3
albert.kao <albert.kao@udngroup.com>
2022-09-21 10:14:39 +0800

    feat: many to many seeding

--- app/app/database/seeders/DatabaseSeeder.php
+++ app/app/database/seeders/DatabaseSeeder.php
@@ -37,6 +37,8 @@ class DatabaseSeeder extends Seeder
             UsersTableSeeder::class,
             BlogPostsTableSeeder::class,
             CommentsTableSeeder::class,
+            TagsTableSeeder::class,
+            BlogPostTagTableSeeder::class
         ]);
 
     }
