a94ad8323e1060916df7e54a16e63c8a2c906635
albert.kao <albert.kao@udngroup.com>
2022-09-22 16:30:16 +0800

    feat: beauty show page layout

--- app/app/resources/views/posts/show.blade.php
+++ app/app/resources/views/posts/show.blade.php
@@ -5,27 +5,37 @@
 @section('content')
 <div class="row">
     <div class="col-8">
-{{-- @if ($post['is_new'])
-<div>A new blog post! Using if</div>
-@elseif(!$post['is_new'])
-<div>Blog post id old! using elseif / else</div>
-@endif
+    {{-- @if ($post['is_new'])
+    <div>A new blog post! Using if</div>
+    @elseif(!$post['is_new'])
+    <div>Blog post id old! using elseif / else</div>
+    @endif
 
-@unless ($post['is_new'])
-<div>It is an old post ... using unless</div>
-@endunless --}}
-
+    @unless ($post['is_new'])
+    <div>It is an old post ... using unless</div>
+    @endunless --}}
+    @if ($post->image)
+    <div style="background-image: url(' {{ $post->image->url() }}'); min-heigth: 500px; color: white; text-align: center; background-attachment: fixed;">
+        <h1 style="padding-top: 100px; text-shadow: 1px 2px #000">
+    @else
         <h1>
+    @endif
             {{ $post->title }}
             <x-badge type='danger' show="{{ now()->diffInMinutes($post->created_at) < 20 }}">
                 New!
             </x-badge>
         </h1>
+    @if ($post->image)
+        </h1>
+    </div>
+    @else
+        </h1>
+    @endif
+        
         <p>{{ $post->content }}</p>
         {{-- <img width="400" heigh="400" src="http://127.0.0.1/storage/{{ $post->image->path }}" alt=""> --}}
         {{-- <img width="400" heigh="400" src="{{ asset($post->image->path) }}" alt=""> --}}
         {{-- <img width="400" heigh="400" src="{{ Storage::url($post->image->path) }}" alt=""> --}}
-        <img width="400" heigh="400" src="{{ $post->image->url() }}" alt="">
         {{-- <p>Added {{ $post->created_at->diffForHumans() }}</p> --}}
         <x-updated :date="$post->created_at" :name="$post->user->name">
             Added
