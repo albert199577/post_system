f73e90fd670ad5024186ac063cf3e79d0a449d39
albert.kao <albert.kao@udngroup.com>
2022-12-02 17:25:18 +0800

    feat: ManyToMany Polymorphic relations

--- app/app/app/Models/BlogPost.php
+++ app/app/app/Models/BlogPost.php
@@ -30,7 +30,7 @@ class BlogPost extends Model
 
     public function tags()
     {
-        return $this->belongsToMany('App\Models\Tag')->withTimestamps();
+        return $this->morphToMany('App\Models\Tag', 'taggable')->withTimestamps();
     }
 
     public function image()
--- app/app/app/Models/Tag.php
+++ app/app/app/Models/Tag.php
@@ -11,6 +11,13 @@ class Tag extends Model
 
     public function blogPosts()
     {
-        return $this->belongsToMany('App\Models\BlogPost')->withTimestamps()->as('tagged');
+        // return $this->belongsToMany('App\Models\BlogPost')->withTimestamps()->as('tagged');
+        return $this->morphedByMany('App\Models\BlogPost', 'taggable')->withTimestamps()->as('tagged');
+    }
+
+    public function comments()
+    {
+        // return $this->belongsToMany('App\Models\BlogPost')->withTimestamps()->as('tagged');
+        return $this->morphedByMany('App\Models\Comment', 'taggable')->withTimestamps()->as('tagged');
     }
 }
