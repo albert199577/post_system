ebae4ba778d16e9fb3e40f8b51550be895bab2c3
albert.kao <albert.kao@udngroup.com>
2022-12-19 11:29:00 +0800

    feat: localization storing translations in JSON

---
+++ app/app/resources/lang/es/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'welcome' => 'iBienvenido to Laravel!',
+    'example_with_value' => 'Ola :name',
+    'plural' => '{0} No commentarios :a|{1} :count commentario :a|[2,*] :count commentarios :a'
+];
--- app/app/resources/views/home/index.blade.php
+++ app/app/resources/views/home/index.blade.php
@@ -3,6 +3,16 @@
 @section('title', 'Home Page')
 
 @section('content')
+<h1>{{ __('messages.welcome') }}</h1>
+<h1>@lang('messages.welcome')</h1>
 
-<h1>Hello world!</h1>
+<p>{{ __('messages.example_with_value', ['name' => 'John']) }}</p>
+
+<p>{{ trans_choice('messages.plural', 0, ['a' => 1]) }}</p>
+<p>{{ trans_choice('messages.plural', 1, ['a' => 1]) }}</p>
+<p>{{ trans_choice('messages.plural', 2, ['a' => 1]) }}</p>
+
+<p>{{ __('Welcome to Laravel!') }}</p>
+<p>{{ __('Hello :name', ['name' => 'Porta']) }}</p>
+
 @endsection
